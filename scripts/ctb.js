13406dca505063c0a0381752cd39944e0178c047
Nick A <jacksin.slate@gmail.com>
2017-06-26 19:47:28 -0400

    buttons sliders highlight class and resized steps

--- scripts/ctb.js
+++ scripts/ctb.js
@@ -67,5 +67,25 @@ $(document).ready(function(){
 	$('.randomize').click(function(){
 		randomizeAll();
 	});
+	var rangeSlider = function(){
+	  var slider = $('.range-slider'),
+	      range = $('.range-slider__range'),
+	      value = $('.range-slider__value');
+	    
+	  slider.each(function(){
+
+	    value.each(function(){
+	      var value = $(this).prev().attr('value');
+	      $(this).html(value);
+	    });
+
+	    range.on('input', function(){
+	      $(this).next(value).html(this.value);
+	    });
+	  });
+	};
+
+	rangeSlider();
+
 
 });
